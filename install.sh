241382ef6c80ea52bcb05e018e294a1d610d018b
Aaron Marburg <amarburg@uw.edu>
2026-07-09 21:41:00 -0700

    Initial adaptation of trisect to 36.5

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@
 # Install script which is added to install.tar.bz2 by "make package"
 
 sudo install -o root -g root -m 644 boot/* /boot
+<<<<<<< HEAD
 sudo find lib/modules/5.15.199-tegra/updates/ -type f -print -exec install -D -o root -g root -m 644 \{\} /\{\} \;
+=======
+sudo find lib/modules/5.15.185-tegra/updates/ -type f -print -exec install -D -o root -g root -m 644 \{\} /\{\} \;
+>>>>>>> 7f7265f (Initial adaptation of trisect to 36.5)
 sudo depmod -a
